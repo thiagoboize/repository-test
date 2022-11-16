cc9ed7fcd18488b8d23eb3e23cc8133f248f8c5d
Thiago <thiago@pop-os.localdomain>
2022-11-16 18:31:22 -0300

    mongo-tst

--- main.js
+++ main.js
@@ -1,9 +1,10 @@
+const mongoose = require('mongoose')
 const dotenv = require("dotenv")
 dotenv.config()
 const express = require('express')
 
 const app = express()
-const PORT = process.env.PORT || 21193;
+const PORT = process.env.PORT_APP || 21193;
 
 app.get('/api', (req, res) => {
     res.send('Hello World!')
@@ -12,3 +13,11 @@ app.get('/api', (req, res) => {
 app.listen(PORT, () => {
     console.log("server running")
 })
+
+mongoose.connect('mongodb+srv://thiagoboize:VRpqfhbhAjj3Fn6v@finance-database.tjl8pvq.mongodb.net/test', {
+
+})
+    .then(() => {
+        console.log("Conectado com o MongoDB");
+    })
+    .catch(err => console.log(err))
