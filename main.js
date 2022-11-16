fb85d55592424e36195ff8ee946f2de72bb3838f
Thiago <thiago@pop-os.localdomain>
2022-11-16 15:35:38 -0300

    teasdsadsa

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ dotenv.config()
 const express = require('express')
 
 const app = express()
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 21193;
 
 app.get('/api', (req, res) => {
     res.send('Hello World!')
