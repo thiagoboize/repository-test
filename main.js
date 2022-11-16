8acf2209dbfee47988f07c6e41eca15d0271b0bb
Thiago <thiago@pop-os.localdomain>
2022-11-16 14:58:01 -0300

    teste2

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 const express = require('express')
 
 const app = express()
-const PORT = process.env.PORT || 3005;
+const PORT = process.env.PORT || 3003;
 
 app.get('/api', (req, res) => {
     res.send('Hello World!')
